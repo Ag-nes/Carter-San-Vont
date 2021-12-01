a5917a4d389df981b3a0c5cb84b8a9775c542faf
Joshua-Barawa <joshua.barawa@student.moringaschool.com>
2021-12-01 15:23:32 +0300

    second push

---
+++ data.js
@@ -0,0 +1,65 @@
+const vegy =[
+{
+    "id":1,
+    "name":"Broccoli",
+    "price": 50,
+    "image":"/images/broccoli.jpg"
+},
+
+{
+    "id":2,
+    "name":"Fresh Cabbage",
+    "price": 60,
+    "image":"/images/fresh-cabbage.jpeg"
+},
+
+{
+    "id":3,
+    "name":"Kales",
+    "price": 50,
+    "image":"/images/Kales.png"
+},
+
+{
+    "id":4,
+    "name":"Spinach",
+    "price": 20,
+    "image":"/images/spinach.jpg"
+}
+];
+
+
+
+const cereals =[
+    {
+        "id":5,
+        "name":"Rice",
+        "price": 90,
+        "image":"/images/rice1.jpg"
+    },
+    
+    {
+        "id":6,
+        "name":"Green Peas",
+        "price": 80,
+        "image":"/images/peas.jpg"
+    },
+    
+    
+    {
+        "id":7,
+        "name":"Sorghum",
+        "price": 50,
+        "image":"/images/sorghum.jpg"
+    },
+    
+    
+    {
+        "id":8,
+        "name":"Oats",
+        "price": 100,
+        "image":"/images/oats.jpg"
+    }
+    
+    
+    ]
